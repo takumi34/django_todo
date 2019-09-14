67ea163a1d7c810650604d92b16262f1f6a97fd8
takumi34 <1shiranui@gmail.com>
2019-09-15 05:20:14 +0900

    update views.py(done read)

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from .models import Todo
+from django.views.generic.list import ListView
+
+
+
+class TodoListView(ListView):
+    model = Todo
+
+    def get_context_date(self, **kwargs):
+        context = super(TodoListView, self).get_context_data(**kwargs)
+        return context
 
 def top(request):
-  return render(request, 'app/top.html')
+  items = Todo.objects.all().order_by('-updated_datetime')
+  return render(request, 'app/top.html', { 'items': items })
 
 
