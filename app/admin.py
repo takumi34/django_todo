28fcca985e8d3a03458c3c259ada6f33f04816eb
takumi34 <1shiranui@gmail.com>
2019-09-15 04:16:03 +0900

    update model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+# Register your models here
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'created_datetime', 'updated_datetime')
+    list_display_links = ('id', 'text')
+
+admin.site.register(Todo, TodoAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    text = models.CharField(max_length=50)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
